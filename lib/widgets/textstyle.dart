5f6cefa038db6497c7eaefae5f6371725699ded1
qaaqil <quvylidh.20200591@iit.ac.lk>
2022-04-02 15:38:09 +0530

    font styles of splash,welcome,signup,signIn

--- lib/widgets/textstyle.dart
+++ lib/widgets/textstyle.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+const TextStyle WelcomeHead = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle GetStarted = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle login = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+);
+const TextStyle wlogin = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+  fontWeight: FontWeight.w800,
+);
+
+const TextStyle signup = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+);
+const TextStyle WelcomeBack = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.black,
+  fontSize: 30,
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle loginButton = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+
+const TextStyle signUpButton = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
