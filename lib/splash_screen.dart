c336ee5b3988728262ab8a7fea8dc6bbfbd58b35
qaaqil <quvylidh.20200591@iit.ac.lk>
2022-04-02 15:35:33 +0530

    splash screen created

---
+++ lib/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:eatright/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => StartState();
+}
+
+class StartState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = Duration(seconds: 4);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => WelcomePageWidget()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initWidget(context);
+  }
+
+  Widget initWidget(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: new Color(0xffF5591F),
+                gradient: LinearGradient(
+                    colors: [(new Color(0xff009688)), new Color(0xff009688)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: Container(
+              child: Image.asset("images/launcher_icon.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
