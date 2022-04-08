a11045bb71215d812cdffd8097a8cdbf03008ace
qaaqil <quvylidh.20200591@iit.ac.lk>
2022-04-09 02:31:57 +0530

    textstyle of allergies and profilepage

---
+++ lib/widgets/textstyle.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+const TextStyle updateProfileAvatar = TextStyle(
+  fontFamily: 'Poppins',
+  color: Color(0xFF009688),
+  fontWeight: FontWeight.w600,
+);
+
+const TextStyle bodytext = TextStyle(
+  fontFamily: 'Poppins',
+  color: Color(0xFF212121),
+);
+
+const TextStyle Continue = TextStyle(
+    fontFamily: 'Poppins',
+    color: Colors.white,
+    fontWeight: FontWeight.w900,
+    fontSize: 23);
+
+const TextStyle whole30 = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle vegan = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle glutenfree = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle ketogenic = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle paleo = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle lowfodmap = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle done = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle Continue2 = TextStyle(
+  fontFamily: 'Poppins',
+  color: Colors.white,
+);
+const TextStyle Checkbox1 = TextStyle(
+  fontFamily: 'Poppins',
+  color: Color(0xFF57636C),
+  fontWeight: FontWeight.w600,
+  fontSize: 22,
+);
+const TextStyle Checkbox2 = TextStyle(
+  fontFamily: 'Poppins',
+  color: Color(0xFF57636C),
+  fontWeight: FontWeight.w700,
+  wordSpacing: 2,
+  fontSize: 26,
+);
