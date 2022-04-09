605c641f83dede85400ba125ea63e0aaf716cc1e
nickjayesh <80024255+nickjayesh@users.noreply.github.com>
2022-04-09 13:48:59 +0530

    Added bottom navigation bar

---
+++ lib/screens/generateMeal/generate.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GenerateMealPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Progress"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/userProfile/userProfile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UserProfile"),
+      ),
+    );
+  }
+}
