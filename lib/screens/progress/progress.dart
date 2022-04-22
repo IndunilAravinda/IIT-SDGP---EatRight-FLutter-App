e69824058467a9acc9cec50ff82f883fd9d46f35
AnjukaS <anjuka.20200828@iit.ac.lk>
2022-04-22 22:04:39 +0530

    merge conflict resolved

--- lib/screens/progress/progress.dart
+++ lib/screens/progress/progress.dart
@@ -1,16 +1,63 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+
+import "package:flutter/material.dart";
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ProgressPage extends StatelessWidget {
+  List<ProgressData> data = [
+    ProgressData(1, 89),
+    ProgressData(2, 78),
+    ProgressData(3, 67),
+    ProgressData(4, 56),
+    ProgressData(5, 55),
+  ];
+
+  ProgressPage({Key? key}) : super(key: key);
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   // TODO: implement build
+
+  // }
+
   @override
   Widget build(BuildContext context) {
+    //var data;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("Progress",
-        style: Theme.of(context).textTheme.headline2,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            "Progress",
+            style: TextStyle(
+                color: Color.fromARGB(255, 52, 54, 54),
+                fontWeight: FontWeight.bold),
+          ),
         ),
-        centerTitle: true,
-      ),
-    );
+        body: Container(
+            child: SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                title: ChartTitle(text: 'Weekly Progress Chart'),
+                legend: Legend(isVisible: true),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<ProgressData, double>>[
+              LineSeries<ProgressData, double>(
+                dataSource: data,
+                xValueMapper: (ProgressData progress, _) => progress.week,
+                yValueMapper: (ProgressData progress, _) => progress.weight,
+                name: 'Progress',
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+              )
+            ])));
   }
+}
+
+class ProgressData {
+  final double week;
+  final double weight;
+  ProgressData(this.week, this.weight);
+
+  // ProgressData(double week, double weight) {
+  //   this.week = week;
+  //   this.weight = weight;
+  // }
 }
